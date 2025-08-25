8e4989cf8ab78b4560d5444efb692efe83c33019
Pedro Negreiros <146891764+pedronegreiros@users.noreply.github.com>
2025-08-25 22:22:29 +0000

    Fiz um desafio da faculdade

--- xadrez.c
+++ xadrez.c
@@ -8,25 +8,39 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    int Torre = 5;   // Torre anda 5 casas
+    int Bispo = 5;   // Bispo anda 5 casas
+    int Rainha = 8;  // Rainha anda 8 casas
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+
+    printf("Movimento do Bispo: 5 casas para cima e direita:\n");
+    int i = 1;
+    while (i <= Bispo) {
+        printf("Cima Direita na diagonal da casa que está\n");
+        i++;
+    }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    printf("Movimento da Torre: 5 casas para a direita:\n");
+    for (int i = 1; i <= Torre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
+    printf("Movimento da Rainha: 8 casas para a esquerda:\n");
+    int n = 1;
+    do {
+        printf("Esquerda\n");
+        n++;
+    } while (n <= Rainha);   
+    
     return 0;
 }
